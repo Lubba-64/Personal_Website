be7979f6a120008976cf959584a4f3c3a782321e
Benjamin Hines <lubba64@gmail.com>
2021-05-11 09:22:09 -0400

    Initial commit

---
+++ Scripts/header.js
@@ -0,0 +1,31 @@
+// adds a header to the document in question automatically
+let Pages = [
+    {P:"/Index.html",N:"Home"},
+    {P:"/Pages/MyWork/MyWork.html",N:"My Work"},
+    {P:"/Pages/WhoAmI/WhoAmI.html",N:"About Me"},
+    {P:"/Pages/Contact/Contact.html",N:"Contact"},
+];
+let Socials = [
+    {S:`https://github.com/Lubba-64`,N:"Github",IMG:"/Images/github.png"}
+]
+function AddHeader(ID){
+    var addto = document.getElementById(ID);
+    var HeaderHTML = `<header class="MainPageHeader" id="MainPageHeader"></header>`;
+    addto.innerHTML = HeaderHTML + addto.innerHTML;
+
+    var HeaderInner = ``;
+    Pages.forEach(page =>{
+        var Text = `<p class="HeaderButtonText">${page.N}</p>`;
+        HeaderInner += `<a href="${page.P}"><button class="HeaderButton">${Text}</button></a>`;
+    });
+    Socials.forEach(social =>{
+        var Image = `<img src="${social.IMG}" alt="!" class="SocialImage">`
+        var Text = `<p class="HeaderButtonText" style="padding-right: 3px;">${social.N}</p>`;
+        HeaderInner += `<a href="${social.S}" target="_blank"><button class="HeaderButton">${Text}${Image}</button></a>`;
+    });
+    var Header = document.getElementById("MainPageHeader");
+    Header.innerHTML = `<div class="HeaderButtonContainer">${HeaderInner}</div>`;
+}
+
+
+AddHeader("BODY");
